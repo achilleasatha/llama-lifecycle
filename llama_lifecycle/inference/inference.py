2b4503afd97d29c5d382477e87a3874ea2fe7934
Achilleas Athanasiou Fragkoulis <achilleasatha@gmail.com>
2024-02-29 22:57:29 +0000

    Refactor

--- llama_lifecycle/inference/inference.py
+++ llama_lifecycle/inference/inference.py
@@ -4,21 +4,20 @@ from transformers import AutoTokenizer, LlamaForCausalLM
 class LlamaChatbot:
     def __init__(
         self,
-        model_path: str = "../../models/llama-2-7b-chat-sft/qlora_adapter",
-        tokenizer_path: str = "../../models/llama-2-7b-chat-hf",
-        device_map: str | None = "cuda:0",
-        max_memory: str | dict | None = "1200MB",
+        model_path: str,
+        tokenizer_path: str,
+        device_map: str | None,
+        max_memory: str | dict | None,
     ):
         self.device_map = device_map
         self.max_memory = max_memory
-
         self.model = LlamaForCausalLM.from_pretrained(
             pretrained_model_name_or_path=model_path,
             device_map=device_map,
             max_memory=max_memory,
         )
         self.tokenizer = AutoTokenizer.from_pretrained(
-            tokenizer_path=tokenizer_path,
+            pretrained_model_name_or_path=tokenizer_path,
             device_map=device_map,
         )
         if self.tokenizer.pad_token is None:
@@ -31,14 +30,15 @@ class LlamaChatbot:
 
         inputs = self.tokenizer(
             formatting_func(prompt), return_tensors="pt"
-        ).input_ids.to(self.max_memory)
+        ).input_ids.to(self.device_map)
         generate_ids = self.model.generate(
             inputs=inputs,
             max_length=max_length,
             pad_token_id=self.tokenizer.pad_token_id,
         )
+
         return self.tokenizer.batch_decode(
-            generate_ids=generate_ids,
+            sequences=generate_ids,
             skip_special_tokens=True,
             clean_up_tokenization_spaces=False,
         )[0]
