0ab5fece4aca500ff5d58d695405d82267e92888
Achilleas Athanasiou Fragkoulis <achilleasatha@gmail.com>
2024-02-29 21:07:41 +0000

    Refactor into class

--- llama_lifecycle/serve/serve.py
+++ llama_lifecycle/serve/serve.py
@@ -1,23 +1,44 @@
 from transformers import AutoTokenizer, LlamaForCausalLM
 
-model = LlamaForCausalLM.from_pretrained(
-    "../../models/llama-2-7b-chat-sft/qlora_adapter",
-    device_map="cuda:0",
-    max_memory="12000MB",
-)
-tokenizer = AutoTokenizer.from_pretrained(
-    "../../models/llama-2-7b-chat-hf", device_map="cuda:0"
-)
-if tokenizer.pad_token is None:
-    tokenizer.pad_token = tokenizer.eos_token
 
-prompt = "Hey, are you conscious? Can you talk to me?"
-inputs = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda:0")
+class LlamaChatbot:
+    def __init__(
+        self,
+        model_path: str = "../../models/llama-2-7b-chat-sft/qlora_adapter",
+        tokenizer_path: str = "../../models/llama-2-7b-chat-hf",
+        device_map: str | None = "cuda:0",
+        max_memory: str | dict | None = "1200MB",
+    ):
+        self.device_map = device_map
+        self.max_memory = max_memory
 
-# Generate
-generate_ids = model.generate(
-    inputs, max_length=300, pad_token_id=tokenizer.pad_token_id
-)
-tokenizer.batch_decode(
-    generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False
-)[0]
+        self.model = LlamaForCausalLM.from_pretrained(
+            pretrained_model_name_or_path=model_path,
+            device_map=device_map,
+            max_memory=max_memory,
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            tokenizer_path=tokenizer_path,
+            device_map=device_map,
+        )
+        if self.tokenizer.pad_token is None:
+            self.tokenizer.pad_token = self.tokenizer.eos_token
+
+    def generate_response(self, prompt: str, max_length: int = 300):
+        def formatting_func(example):
+            text = f"\n ### USER: {example}"
+            return text
+
+        inputs = self.tokenizer(
+            formatting_func(prompt), return_tensors="pt"
+        ).input_ids.to(self.max_memory)
+        generate_ids = self.model.generate(
+            inputs=inputs,
+            max_length=max_length,
+            pad_token_id=self.tokenizer.pad_token_id,
+        )
+        return self.tokenizer.batch_decode(
+            generate_ids=generate_ids,
+            skip_special_tokens=True,
+            clean_up_tokenization_spaces=False,
+        )[0]
