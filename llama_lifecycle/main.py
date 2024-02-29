09d0e67d14744bbc6805228232734e5bb2d51d51
Achilleas Athanasiou Fragkoulis <achilleasatha@gmail.com>
2024-02-29 22:58:18 +0000

    Instantiating bot and running service

--- llama_lifecycle/main.py
+++ llama_lifecycle/main.py
@@ -1,5 +1,9 @@
 """Entrypoint module."""
 
+import uvicorn
+from api.chatbot_api import ChatbotAPI
+from inference.inference import LlamaChatbot
+
 from config import load_config_from_yaml
 
 
@@ -11,11 +15,18 @@ def main(config=None):
     app_name = config.app.app_name
     debug = config.app.debug
     log_level = config.app.log_level
-
-    # Your application logic using the configuration settings
     print(f"Application Name: {app_name}")
     print(f'Debug Mode: {"Enabled" if debug else "Disabled"}')
     print(f"Log Level: {log_level}")
+
+    chatbot = LlamaChatbot(
+        model_path=config.inference.model_path,
+        tokenizer_path=config.inference.tokenizer_path,
+        device_map=config.inference.device_map,
+        max_memory=config.inference.max_memory,
+    )
+    chatbot_api = ChatbotAPI(chatbot=chatbot)
+    uvicorn.run(chatbot_api.app, host=config.api.host, port=config.api.port)
 
 
 if __name__ == "__main__":
